55357c04646a61d20bbf4518f0b7019ab5c858f9
maulikkanani <kanani.maulikb@gmail.com>
2014-03-07 17:43:38 +0530

    03-07-2014-database

--- laraveltest.sql
+++ laraveltest.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 05, 2014 at 07:54 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Mar 07, 2014 at 12:10 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `laraveltest`
 --
+CREATE DATABASE IF NOT EXISTS `laraveltest` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `laraveltest`;
 
 -- --------------------------------------------------------
 
@@ -70,22 +72,24 @@ INSERT INTO `nerds` (`id`, `name`, `email`, `nerd_level`, `created_at`, `updated
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
+  `firstname` varchar(150) NOT NULL,
   `username` varchar(32) NOT NULL,
   `email` varchar(320) NOT NULL,
+  `lastname` varchar(150) DEFAULT NULL,
   `password` varchar(64) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Chris Sevilleja', 'sevilayha', 'chris@scotch.io', '$2y$10$iBzAQ6D9sKhJHahvuHe.Jud/45jWlaHCBjTwRczqWopGYRepbPUru', '2014-03-02 01:23:49', '2014-03-02 01:23:49'),
-(2, 'Maulik kanai', 'test', 'mk@mk.com', '$2y$10$aKN0ohIwgDF1yzhSusOZkedBNBVsehP06IknOqy0QidDTlvrTv/R.', '2014-03-02 01:23:49', '2014-03-02 01:23:49');
+INSERT INTO `users` (`id`, `firstname`, `username`, `email`, `lastname`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Chris Sevilleja', 'sevilayha', 'chris@scotch.io', NULL, '$2y$10$iBzAQ6D9sKhJHahvuHe.Jud/45jWlaHCBjTwRczqWopGYRepbPUru', '2014-03-02 01:23:49', '2014-03-02 01:23:49'),
+(2, 'Maulik kanai', 'test', 'mk@mk.com', NULL, '$2y$10$aKN0ohIwgDF1yzhSusOZkedBNBVsehP06IknOqy0QidDTlvrTv/R.', '2014-03-02 01:23:49', '2014-03-02 01:23:49');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
